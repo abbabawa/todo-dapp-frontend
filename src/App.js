2366af0c52c5ab96e6121777ee2a3acd846fc821
abbabawa <abbabawa6@gmail.com>
2022-11-10 16:56:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import abi from "./utils/SolidityTodoApp.json";
+import { useEffect } from "react";
+import { useState } from "react";
+
+import { ethers } from "ethers";
+import styled from "styled-components";
+
+function App() {
+  const contractAddress = "0x1bdA382C0abae3CbaA7c1eE24DC9992e8B9E75E2";
+  const contractABI = abi.abi;
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getTodos();
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    connectWallet();
+    // getWaves()
+  }, []);
+
+  const getTodos = async () => {console.log("called")
+    const { ethereum } = window;
+
+    try {
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const todoContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        // await todoContract.addTodo("First todo")
+        const waves = await todoContract.getTodos();
+console.log(waves)
+        const wavesCleaned = waves.map((wave) => {
+          return {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          };
+        });
+
+        // setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+  return (
+    <Container>
+      show
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: 'red';
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
